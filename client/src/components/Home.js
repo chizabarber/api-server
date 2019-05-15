4a56555b4e7226d01be4206b4a77a5517912459b
Chiza Barber <chiza.alba@gmail.com>
2019-05-15 08:28:18 -0500

    edited footer

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,39 +8,47 @@ const Home = () => {
             className='center-align'
             style={{ marginTop: '200px' }}
         >
-            <h3
-                style={{
-                    color: '#367da2'
-                }}
-            >
-                A Home for Code
-            </h3>
-            <hr style={{
-                    width: '25%',
-                    marginTop: '30px',
-                    marginBottom: '30px'
-                }} />
-            <p>
-                This sister site to 
-                <a 
-                    href='http://www.chizabarber.com'
-                    style={{ padding: '5px', color: '#367da2' }}
+            <div style={{ height: '62vh' }}>
+                <h3
+                    style={{
+                        color: '#367da2'
+                    }}
                 >
-                    ChizaBarber.com
-                </a>
-                will serve as a space for my projects and
-                experiments as I continue on my coding journey.
-            </p>
-            {/* <div
+                    A Home for Code
+                </h3>
+                <hr style={{
+                        width: '25%',
+                        marginTop: '30px',
+                        marginBottom: '30px'
+                    }} />
+                <p>
+                    This sister site to 
+                    <a 
+                        href='http://www.chizabarber.com'
+                        style={{ padding: '5px', color: '#367da2' }}
+                    >
+                        ChizaBarber.com
+                    </a>
+                    will serve as a space for my projects and
+                    experiments as I continue on my coding journey.
+                </p>
+            </div>
+            <div
                 style={{
                     backgroundColor: '#367da2',
-                    height: '6%',
-                    paddingTop: '1%',
+                    height: '50px',
+                    marginTop: '3%',
                     color: 'white'
                 }}
             >
-                Copyright 2019 © Chiza Barber
-            </div> */}
+                <p style={{
+                    position: 'relative',
+                    top: '50%',
+                    transform: 'translateY(-50%)'
+                }}>
+                    Copyright 2019 © Chiza Barber
+                </p>
+            </div>
         </div>
     )
 }
