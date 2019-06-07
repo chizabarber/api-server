18d5dc48c492a769251879427065664b468f23f0
Chiza Barber <chiza.alba@gmail.com>
2019-06-07 14:56:24 -0500

    fixing auth issue

--- client/src/components/dash/Dashboard.js
+++ client/src/components/dash/Dashboard.js
@@ -1,7 +1,6 @@
 // ------------------------------------------------------
 import React, { Component } from 'react'
 // ------------------------------------------------------
-import keys from '../../config/keys'
 import requireAuth from '../hocs/requireAuth'
 import AdminDash from './AdminDash'
 import UserDash from './UserDash'
@@ -10,7 +9,7 @@ import UserDash from './UserDash'
 class Dashboard extends Component {
     render () {
         const auth = this.props.auth
-        if (auth.googleId === keys.adminID) {
+        if (auth.googleId === '104115147170088126221') {
             return (
                 <AdminDash />
             )
